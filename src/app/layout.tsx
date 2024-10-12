caead2a8a5ad4b0dd35cb6da857934ad26555af7
Sophie <sls701.818@gmail.com>
2024-10-12 15:14:58 -0700

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
